6e0c2b4a00f46cffcde679046f720c2fb87c86d0
Jiafeng Du <dujiafengdave@gmail.com>
2023-06-13 00:28:42 +0000

    Finish custom template tags

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,18 +1,42 @@
 from django import template
 from django.utils.html import format_html
 
+from blog.models import Post
 from django.contrib.auth import get_user_model
 user_model = get_user_model()
 
 register = template.Library()
 
+@register.inclusion_tag("blog/post-list.html")
+def recent_posts(post):
+  posts = Post.objects.exclude(pk=post.pk)[0:5]
+  return {"posts": posts, "title": "Recent Posts"}
+
+
+@register.simple_tag
+def col(extra_classes=""):
+  return format_html('<div class="col {}"', extra_classes)
+
+@register.simple_tag
+def endcol():
+  return format_html('</div>')
+
+@register.simple_tag
+def row(extra_classes=""):
+  return format_html('<div class="row {}">', extra_classes)
+
+@register.simple_tag
+def endrow():
+  return format_html('</div>')
+
 @register.filter
 def author_details(author, current_user=None):
+
   if not isinstance(author, user_model):
     return ""
   
   if author == current_user:
-    return format_html('<strong>me</string>')  
+    return format_html('<strong>me</string>')
   
   if author.first_name and author.last_name:
     name = format_html("{} {}", author.first_name, author.last_name)
@@ -27,3 +51,4 @@ def author_details(author, current_user=None):
     suffix = ""
 
   return format_html("{}{}{}", prefix, name, suffix)
+
