6a6d2343faa8e3ff41d8f25e4cdfb47cce326178
Jiafeng Du <dujiafengdave@gmail.com>
2023-10-13 20:57:25 +0000

    Finish simplejwt

--- blango/settings.py
+++ blango/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 from configurations import Configuration, values
 import dj_database_url
+from datetime import timedelta
 
 class Dev(Configuration):
     # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -226,6 +227,7 @@ class Dev(Configuration):
         "rest_framework.authentication.BasicAuthentication",
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
       ],
       "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticatedOrReadOnly",
@@ -256,6 +258,12 @@ class Dev(Configuration):
             "Token": {"type": "apiKey", "name": "Authorization", "in": "header"},
             "Basic": {"type": "basic"},
         },
+    }
+
+    # SimpleJWT settings
+    SIMPLE_JWT = {
+        "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+        "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     }
 
 class Prod(Dev):
--- blog/api/urls.py
+++ blog/api/urls.py
@@ -3,6 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 import os
@@ -16,6 +18,8 @@ router.register("posts", PostViewSet)
 urlpatterns = [
   path("auth/", include("rest_framework.urls")),
   path("token-auth/", views.obtain_auth_token),
+  path("jwt/", TokenObtainPairView.as_view(), name="jwt_obtain_pair"),
+  path("jwt/refresh", TokenRefreshView.as_view(), name="jwt_refresh"),
   path("users/<str:email>", UserDetail.as_view(), name="api_user_detail"),
 ]
 
