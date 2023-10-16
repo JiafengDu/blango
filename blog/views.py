0551988d3876b0e1291be8915b02f43c426a3b12
Jiafeng Du <dujiafengdave@gmail.com>
2023-10-16 22:00:32 +0000

    Finish JavaScript intro and fundamentals

--- blog/views.py
+++ blog/views.py
@@ -10,6 +10,9 @@ from blog.forms import CommentForm
 logger = logging.getLogger(__name__)
 
 # Create your views here.
+def post_table(request):
+  return render(request, "blog/post-table.html")
+
 def get_ip(request):
   from django.http import HttpResponse
   return HttpResponse(request.META['REMOTE_ADDR'])
